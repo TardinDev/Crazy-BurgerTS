ab0d811409d380366d18d6d8216cceaab4c51b4a
TardinDev <tardindavy@gmail.com>
2024-10-16 23:11:53 +0200

    ⚡️ LoginPage > update

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,11 +1,50 @@
+import { useState } from "react"
 
 
 
 
 export default function LoginPage() {
 
-    
+
+    const [inputName, setInputName] = useState("")
+
+
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+
+          e.preventDefault()
+    }
+
+
+
+    const handleChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+          
+          setInputName(e.target.value)
+
+    }
+
+
+
+
   return (
-    <div>LoginPage</div>
+
+    <div>
+
+        <h1>Crazy-burger</h1>
+
+
+
+        <form action="submit" onSubmit={handleSubmit}>
+           <input 
+              type="text" 
+              placeholder="write your name"
+              required
+              value={inputName}
+              onChange={handleChange}/>
+           <button>get in your account</button>
+        </form>
+
+    </div>
+
   )
 }

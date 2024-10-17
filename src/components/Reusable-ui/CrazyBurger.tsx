f2550f336258b11a3f2ba4ac7cbd05a869cda9f9
TardinDev <tardindavy@gmail.com>
2024-10-17 11:30:26 +0200

    ✨Reusable > add crazy burger component

---
+++ src/components/Reusable-ui/CrazyBurger.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components"
+import cbLogo from "../../../public/logo-orange.png"
+
+
+type CrazyBurgerType = {
+
+    imgWidth:string,
+    imgHeight:string,
+    label:string
+}
+
+
+export default function CrazyBurger({imgWidth, imgHeight, label}: CrazyBurgerType) {
+
+
+  return (
+
+      <CrazyBurgerStyle imgWidth={imgWidth} imgHeight={imgHeight} label={label}  >
+
+        <h1>Crazy</h1>
+          <img src={cbLogo} alt="burgerimage"/>
+        <h1>Burger</h1>
+
+      </CrazyBurgerStyle>
+
+  )
+}
+
+const CrazyBurgerStyle = styled.div<CrazyBurgerType>`
+
+
+      background-color:#EB8317;
+      display:flex;
+      align-items:center;
+      gap:10px;
+
+      h1 {
+        font-Size:${props => props.label}
+      }
+
+      img {
+        width:${props => props.imgWidth};
+        height:${props => props.imgHeight};
+        
+      }
+
+
+`

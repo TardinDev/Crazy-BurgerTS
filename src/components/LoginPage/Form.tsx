0ff86f751e1f3e0061f5dd4695faa5f8b92d7fbb
TardinDev <tardindavy@gmail.com>
2024-10-17 12:05:47 +0200

    💄 Form > update style

--- src/components/LoginPage/Form.tsx
+++ src/components/LoginPage/Form.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import styled from "styled-components"
 
 
 
@@ -26,15 +27,49 @@ export default function Form() {
 
   return (
 
-    <form action="submit" onSubmit={handleSubmit}>
+    <FormStyle action="submit" onSubmit={handleSubmit}>
+
            <input 
               type="text" 
-              placeholder="write your name"
+              placeholder="...write your name..."
               required
               value={inputName}
               onChange={handleChange}/>
            <button>get in your account</button>
-    </form>
+
+    </FormStyle>
 
   )
 }
+
+const FormStyle = styled.form`
+
+      display:flex;
+      flex-direction:column;
+      gap:10px;
+
+      input {
+        width:20rem;
+        height:2rem;
+
+        border:0.5px solid orange;
+        border-radius:5px;
+
+        padding:0px 5px;
+        text-align:center;
+      }
+
+      button {
+        width:20.70rem;
+        height:2rem;
+
+        border:0.5px solid green;
+        border-radius:5px;
+
+        padding:0px 5px;
+        text-align:center;
+      }
+
+     
+
+`

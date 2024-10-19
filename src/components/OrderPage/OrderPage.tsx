a720f7d29ab038ff208963ff5ba7ac93c75d5b4b
TardinDev <tardindavy@gmail.com>
2024-10-19 18:28:38 +0200

    ⚡️ OrderPage > refactoring

--- src/components/OrderPage/OrderPage.tsx
+++ src/components/OrderPage/OrderPage.tsx
@@ -1,11 +1,47 @@
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import HeaderOrder from "./HeaderOrder";
+import Menu from "./Menu";
 
 
 
 
 export default function OrderPage() {
 
-    
+    const {inputName} = useParams();
+
+
   return (
-    <div>OrderPage</div>
+
+    <OrderPageStyle>
+
+      <HeaderOrder inputName={inputName}/>
+
+      <Menu/>
+       
+
+    </OrderPageStyle>
+
+
   )
 }
+
+const OrderPageStyle = styled.div`
+
+     background-color:#EB8317;
+     height:100vh;
+     padding:2rem 4.5rem;
+
+     display:flex;
+     flex-direction:column;
+
+     box-sizing: border-box; 
+ 
+    
+  
+     
+  
+
+     
+
+`

a8e5e22faef6b043467367e04843327ec634fe68
TardinDev <tardindavy@gmail.com>
2024-10-19 18:40:07 +0200

    ✨ Create MenuRightSide

---
+++ src/components/OrderPage/MenuRightSide.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+
+
+export default function MenuRightSide() {
+
+
+  return (
+
+    <MenuRightSideStyle>
+
+          MenuRightSide
+
+    </MenuRightSideStyle>
+
+  )
+}
+
+const MenuRightSideStyle = styled.div`
+
+      
+       background-color:#347928;
+
+`

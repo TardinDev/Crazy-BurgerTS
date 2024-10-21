e251612b525b4bbe1ce602f8ae8a707e20b2f296
TardinDev <tardindavy@gmail.com>
2024-10-21 09:46:17 +0200

    ⚡️ orderPage > create and add Context

--- src/components/OrderPage/OrderPage.tsx
+++ src/components/OrderPage/OrderPage.tsx
@@ -2,46 +2,39 @@ import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import HeaderOrder from "./HeaderOrder";
 import Menu from "./Menu";
-
+import { useState } from "react";
+import orderContext from "../../context/orderContext";
 
 
 
 export default function OrderPage() {
 
-    const {inputName} = useParams();
+  const { inputName } = useParams<{ inputName: string }>(); 
 
+  const [isActiveBtn, setIsActiveBtn] = useState<boolean>(false);
+
+  const orderContextValue = {
+    isActiveBtn,
+    setIsActiveBtn,
+  };
 
   return (
-
     <OrderPageStyle>
-
-      <HeaderOrder inputName={inputName}/>
-
-      <Menu/>
-       
-
+      <orderContext.Provider value={orderContextValue}>
+        <HeaderOrder inputName={inputName || "Guest"} /> 
+        <Menu />
+      </orderContext.Provider>
     </OrderPageStyle>
-
-
-  )
+  );
 }
 
+
+// Styles du composant OrderPage
 const OrderPageStyle = styled.div`
-
-     background-color:#EB8317;
-     height:100vh;
-     padding:2rem 4.5rem;
-
-     display:flex;
-     flex-direction:column;
-
-     box-sizing: border-box; 
- 
-    
-  
-     
-  
-
-     
-
-`
+  background-color: #eb8317;
+  height: 100vh;
+  padding: 2rem 4.5rem;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+`;

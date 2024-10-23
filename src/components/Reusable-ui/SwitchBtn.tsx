24433c9d151fe99b64d03b266899fc7d1580f6ec
TardinDev <tardindavy@gmail.com>
2024-10-23 10:37:36 +0200

    💄 SwitchBtn > improve ui style

--- src/components/Reusable-ui/SwitchBtn.tsx
+++ src/components/Reusable-ui/SwitchBtn.tsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { useContext } from "react";
 import { FaCircle } from "react-icons/fa";
 import styled from "styled-components";
+import orderContext from "../../context/orderContext"; 
 
 type SwitchBtnType = {
   isActiveBtn: boolean;
 };
 
 export default function SwitchBtn() {
-  const [isActiveBtn, setIsActiveBtn] = useState(false);
+
+  const { isActiveBtn, setIsActiveBtn } = useContext(orderContext);
 
   const handleSwitchBtn = () => {
-    setIsActiveBtn(!isActiveBtn);
+    setIsActiveBtn(!isActiveBtn); 
   };
 
   return (
@@ -26,12 +28,13 @@ export default function SwitchBtn() {
 const SwitchBtnStyle = styled.div<SwitchBtnType>`
   display: flex;
   justify-content: space-between;
+ 
   align-items: center;
   background-color: #000;
   border: 2px solid ${(props) => (props.isActiveBtn ? "lightgrey" : "#EB8317")};
-  border-radius: 20px;
-  width: 16.5rem;
-  padding: 0.3rem 0.1rem;
+  border-radius: 18px;
+  width: 16.8rem;
+  padding: 0.3rem 0rem;
   cursor: pointer;
   position: relative;
   overflow: hidden;
@@ -39,7 +42,7 @@ const SwitchBtnStyle = styled.div<SwitchBtnType>`
   .circle {
     position: absolute;
     top: 50%;
-    left: ${(props) => (props.isActiveBtn ? "calc(100% - 45px)" : "10px")}; /* Move to right when active */
+    left: ${(props) => (props.isActiveBtn ? "calc(100% - 45px)" : "10px")};
     transform: translateY(-50%);
     transition: left 0.5s ease-in-out;
     display: flex;
@@ -50,9 +53,9 @@ const SwitchBtnStyle = styled.div<SwitchBtnType>`
   h1 {
     color: ${(props) => (props.isActiveBtn ? "#EB8317" : "lightgrey")};
     transition: color 0.3s ease;
-    margin-left: 20px; /* Reduce space between the circle and text */
-    text-align: ${(props) => (props.isActiveBtn ? "left" : "right")}; /* Text alignment based on state */
-    width: 100%; /* Allow text to adjust dynamically */
-    padding-right: 35px; /* Reduced padding-right to make space for the circle */
+    margin-left: 20px;
+    text-align: ${(props) => (props.isActiveBtn ? "left" : "right")};
+    width: 100%;
+    padding-right: 35px;
   }
 `;
